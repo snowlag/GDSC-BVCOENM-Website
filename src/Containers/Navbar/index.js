4e121aed272dd43b765fd00a6ded3b2330732ec8
=Snowlag <=ankitjoshicr7@gmail.com>
2021-11-04 14:10:14 +0530

    Small changes

--- src/Containers/Navbar/index.js
+++ src/Containers/Navbar/index.js
@@ -22,18 +22,9 @@ export default function Header() {
       <Nav.Link href="/">Home</Nav.Link>
       <Nav.Link href="/events">Events</Nav.Link>
       <Nav.Link href="/teams">Teams</Nav.Link>
-      {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown> */}
     </Nav>
     <Nav>
-      <Container>
       <Nav.Link href="/contact">Contact us</Nav.Link>
-      </Container>
     </Nav>
   </Navbar.Collapse>
 </Navbar>
